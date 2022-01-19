8944a48284344fcff92ac79341599c06cddd4e7c
eric_wang <jadeface.scholar@gmail.com>
2022-01-19 22:34:33 +0800

    perf: perf code design

--- src/humanize.js
+++ src/humanize.js
@@ -3,50 +3,91 @@
 const MS_PER_SECOND = 1000;
 const MS_PER_MINUTE = 60 * MS_PER_SECOND;
 const MS_PER_HOUR = 60 * MS_PER_MINUTE;
-
-const humanizeActual = msActual => {
-  let unit;
-  if (msActual < MS_PER_SECOND) {
-    msActual = Math.round(msActual);
-    unit = 'ms';
-  } else if (msActual < MS_PER_MINUTE) {
-    msActual = (msActual / MS_PER_SECOND).toFixed(1);
-    unit = 'secs';
-  } else if (msActual < MS_PER_HOUR) {
-    msActual = (msActual / MS_PER_MINUTE).toFixed(1);
-    unit = 'mins';
-  } else {
-    msActual = (msActual / MS_PER_HOUR).toFixed(1);
-    unit = 'hours';
-  }
-
-  if (msActual % 1 === 0) {
-    return `${Math.round(msActual)} ${unit}`;
-  }
-  return `${msActual} ${unit}`;
+const MS_MODE = {
+  ACTUAL: 'ACTUAL',
+  REMAINING: 'REMAINING'
+};
+const ACTUAL_MS_UNIT = {
+  MS: 'ms',
+  SECS: 'secs',
+  MINS: 'mins',
+  HOURS: 'hours'
+};
+const REMAINING_MS_UNIT = {
+  MS: 'ms',
+  S: 's',
+  M: 'm',
+  H: 'h'
 };
 
-const humanizeRemaining = (msElapsed, msEstimated) => {
+const getMsUnit = (ms, msMode) => {
   let unit;
+  if (ms < MS_PER_SECOND) {
+    unit = msMode === MS_MODE.ACTUAL ? ACTUAL_MS_UNIT.MS : REMAINING_MS_UNIT.MS;
+  } else if (ms < MS_PER_MINUTE) {
+    unit =
+      msMode === MS_MODE.ACTUAL ? ACTUAL_MS_UNIT.SECS : REMAINING_MS_UNIT.S;
+  } else if (ms < MS_PER_HOUR) {
+    unit =
+      msMode === MS_MODE.ACTUAL ? ACTUAL_MS_UNIT.MINS : REMAINING_MS_UNIT.M;
+  } else {
+    unit =
+      msMode === MS_MODE.ACTUAL ? ACTUAL_MS_UNIT.HOURS : REMAINING_MS_UNIT.H;
+  }
+
+  return unit;
+};
+
+const getActualMs = msActual => {
+  if (msActual < MS_PER_SECOND) {
+    msActual = Math.round(msActual);
+  } else if (msActual < MS_PER_MINUTE) {
+    msActual = (msActual / MS_PER_SECOND).toFixed(1);
+  } else if (msActual < MS_PER_HOUR) {
+    msActual = (msActual / MS_PER_MINUTE).toFixed(1);
+  } else {
+    msActual = (msActual / MS_PER_HOUR).toFixed(1);
+  }
+
+  return msActual;
+};
+
+const getRemainingMS = (msElapsed, msEstimated) => {
   if (msEstimated < MS_PER_SECOND) {
     msElapsed = Math.round(msElapsed);
     msEstimated = Math.round(msEstimated);
-    unit = 'ms';
   } else if (msEstimated < MS_PER_MINUTE) {
     msElapsed = (msElapsed / MS_PER_SECOND).toFixed(1);
     msEstimated = (msEstimated / MS_PER_SECOND).toFixed(1);
-    unit = 's';
   } else if (msEstimated < MS_PER_HOUR) {
     msElapsed = (msElapsed / MS_PER_MINUTE).toFixed(1);
     msEstimated = (msEstimated / MS_PER_MINUTE).toFixed(1);
-    unit = 'm';
   } else {
     msElapsed = (msElapsed / MS_PER_HOUR).toFixed(1);
     msEstimated = (msEstimated / MS_PER_HOUR).toFixed(1);
-    unit = 'h';
   }
 
-  return `${msElapsed}${unit}, estimated ${msEstimated}${unit}`;
+  return {
+    _msElapsed: msElapsed,
+    _msEstimated: msEstimated
+  };
+};
+
+const humanizeActual = msActual => {
+  const unit = getMsUnit(msActual, MS_MODE.ACTUAL);
+  const _msActual = getActualMs(msActual);
+
+  if (msActual % 1 === 0) {
+    return `${Math.round(_msActual)} ${unit}`;
+  }
+  return `${_msActual} ${unit}`;
+};
+
+const humanizeRemaining = (msElapsed, msEstimated) => {
+  const unit = getMsUnit(msEstimated, MS_MODE.REMAINING);
+  const { _msElapsed, _msEstimated } = getRemainingMS(msElapsed, msEstimated);
+
+  return `${_msElapsed}${unit}, estimated ${_msEstimated}${unit}`;
 };
 
 module.exports = {
